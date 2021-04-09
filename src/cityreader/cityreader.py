8f56de9147b52b17153ffb3e867cd70b0e8ba4f3
ik-okoro <ik.o.okoro@gmail.com>
2021-04-09 11:36:09 -0400

    MVP cityreader
    Completed MVP section of cities problem. Return for stretch if time permits.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  
+  def __str__(self):
+    return f"{self.name} @ latitude {self.lat} and longitude {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -21,6 +31,18 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open("cities.csv", mode = "r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+
+    for row in csv_reader:
+      if line_count == 0:
+        line_count +=1
+        continue
+
+      cities.append(City(row["city"], row["lat"], row["lng"]))
+      line_count +=1
     
     return cities
 
@@ -29,6 +51,8 @@ cityreader(cities)
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
     print(c)
+
+# print(len(cities))
 
 # STRETCH GOAL!
 #
